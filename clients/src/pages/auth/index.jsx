8634c4af3cc6fe4eaf7b5304d068120596f1ca79
dgdurgesh <142913115+durgesh-h@users.noreply.github.com>
2024-07-17 11:11:37 +0530

    Login page UI changed

--- clients/src/pages/auth/index.jsx
+++ clients/src/pages/auth/index.jsx
@@ -1,4 +1,5 @@
 import Background from "@/assets/bhellogo.png";
+import meeting2 from '@/assets/meeting2.jpeg'
 import Victory from "@/assets/victory.svg";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -9,7 +10,7 @@ import { LOGIN_ROUTE, SIGNUP_ROUTE } from "@/utils/constants";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
-
+import meeting from '../../assets/meeting.jpeg'
 function Auth() {
   const navigate = useNavigate();
   const {setUserInfo}= useAppStore();
@@ -75,15 +76,24 @@ function Auth() {
   };
 
   return (
+    
     <div className="h-[100vh] w-[100vw] flex items-center justify-center">
-      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+      <img src={meeting} className=" md:flex justify-center items-center w-full hidden h-screen overflow-hidden relative"/>
+      <img src={meeting2} className=" flex justify-center items-center w-full md:hidden h-screen overflow-hidden relative"/>
+
+      <div className=" absolute xl:flex mb-[75vh]">
+          <img src={Background} alt="background login" className="md:h-[110px] h-28 rounded-md" />
+          
+
+        </div>
+      <div className="h-[60vh] mt-40 absolute bg-white bg-opacity-90 border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-1">
         <div className="flex flex-col gap-10 items-center justify-center">
           <div className="flex flex-col items-center justify-center ">
             <div className="flex items-center justify-center">
               <h1 className="text-5xl items-center font-bold md:text-6xl">
                 Welcome
               </h1>
-              <img src={Victory} alt="victory emoji" className="h-[100px]" />
+              {/* <img src={Victory} alt="victory emoji" className="h-[100px]" /> */}
             </div>
             <p className="font-medium text-center">
               Fill in the details to get started with the Discussion Forum App!
@@ -116,14 +126,14 @@ function Auth() {
                 <Input
                   placeholder="Email"
                   type="email"
-                  className="rounded-full p-6"
+                  className="rounded-full p-6 border-blue-800"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
                 <Input
                   placeholder="Password"
                   type="password"
-                  className="rounded-full p-6"
+                  className="rounded-full p-6  border-blue-800"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
@@ -139,21 +149,21 @@ function Auth() {
                 <Input
                   placeholder="Email"
                   type="email"
-                  className="rounded-full p-6"
+                  className="rounded-full p-6  border-blue-800"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
                 <Input
                   placeholder="Password"
                   type="password"
-                  className="rounded-full p-6"
+                  className="rounded-full p-6  border-blue-800"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
                 <Input
                   placeholder="Confirm Password"
                   type="password"
-                  className="rounded-full p-6"
+                  className="rounded-full p-6  border-blue-800"
                   value={confirmPassword}
                   onChange={(e) => setConfirmPassword(e.target.value)}
                 />
@@ -164,9 +174,9 @@ function Auth() {
             </Tabs>
           </div>
         </div>
-        <div className="hidden xl:flex justify-center items-center">
+        {/* <div className="hidden xl:flex justify-center items-center">
           <img src={Background} alt="background login" className="h-[200px]" />
-        </div>
+        </div> */}
       </div>
     </div>
   );
---
+++ clients/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -0,0 +1,8 @@
+
+function MessageContainer() {
+  return (
+    <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[80vw] w-full "></div>
+  )
+}
+
+export default MessageContainer
