47e7ed4f12b6734e575ddfc672ef1b61fde7153f
dgdurgesh <142913115+durgesh-h@users.noreply.github.com>
2024-07-19 01:09:03 +0530

    File Upload & Download functionality is implemented

--- clients/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ clients/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -1,5 +1,9 @@
+import { Input } from "@/components/ui/input";
 import { useSocket } from "@/context/SocketContext";
+import { apiClient } from "@/lib/api-client";
 import { useAppStore } from "@/store";
+import { UPLOAD_FILE_ROUTE } from "@/utils/constants";
+import { data } from "autoprefixer";
 import EmojiPicker from "emoji-picker-react";
 import { useEffect, useRef, useState } from "react";
 import { GrAttachment } from "react-icons/gr";
@@ -8,8 +12,15 @@ import { RiEmojiStickerLine } from "react-icons/ri";
 
 function MessageBar() {
   const emojiRef = useRef();
+  const fileInputRef = useRef();
   const socket = useSocket();
-  const { selectedChatType, selectedChatData, userInfo } = useAppStore();
+  const {
+    selectedChatType,
+    selectedChatData,
+    userInfo,
+    setIsUploading,
+    setFileUploadProgress,
+  } = useAppStore();
   const [message, setMessage] = useState("");
   const [emojiPickerOpen, setEmojiPickerOpen] = useState(false);
   useEffect(() => {
@@ -29,7 +40,7 @@ function MessageBar() {
   };
 
   const handleSendMessage = async () => {
-    if(selectedChatType === "contact"){
+    if (selectedChatType === "contact") {
       socket.emit("sendMessage", {
         sender: userInfo.id,
         content: message,
@@ -39,6 +50,46 @@ function MessageBar() {
       });
     }
   };
+
+  const handleAttachmentClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleAttachmentChange = async (event) => {
+    try {
+      const file = event.target.files[0];
+      if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+        setIsUploading(true);
+        const response = await apiClient.post(UPLOAD_FILE_ROUTE, formData, {
+          withCredentials: true,
+          onUploadProgress: (data) => {
+            setFileUploadProgress(Math.round((100 * data.loaded) / data.total));
+          },
+        });
+        if (response.status === 200 && response.data) {
+          setIsUploading(false);
+          if (selectedChatType === "contact") {
+            socket.emit("sendMessage", {
+              sender: userInfo.id,
+              content: undefined,
+              recipient: selectedChatData._id,
+              messageType: "file",
+              fileUrl: response.data.filePath,
+            });
+          }
+        }
+      }
+      console.log({ file });
+    } catch (error) {
+      setIsUploading(false);
+      console.log({ error });
+    }
+  };
+
   return (
     <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-6">
       <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-5">
@@ -49,9 +100,18 @@ function MessageBar() {
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all">
+        <button
+          className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
+          onClick={handleAttachmentClick}
+        >
           <GrAttachment className="text-2xl" />
         </button>
+        <input
+          type="file"
+          className="hidden"
+          ref={fileInputRef}
+          onChange={handleAttachmentChange}
+        />
         <div className="relative">
           <button
             className="text-neutral-500 focus:border-none focus:outline-none focus:text-white duration-300 transition-all"
--- clients/src/store/slices/chat-slice.js
+++ clients/src/store/slices/chat-slice.js
@@ -1,41 +1,49 @@
-export const createChatSlice = (set, get)=> ({
-    selectedChatType: undefined,
-    selectedChatData: undefined,
-    selectedChatMessages:[],
-    directMessagesContacts:[],
-    setSelectedChatType:(selectedChatType)=> set({selectedChatType}),
-    setSelectedChatData: (selectedChatData)=> set({selectedChatData}),
-    setSelectedChatMessages:(selectedChatMessages)=> 
-        set({selectedChatMessages}),
-    setDirectMessagesContacts:(directMessagesContacts)=> 
-        set({directMessagesContacts}),
-    closeChat:()=>
-        set({
-            selectedChatData: undefined,
-            selectedChatType:undefined,
-            selectedChatMessages: [],
-        }),
+export const createChatSlice = (set, get) => ({
+  selectedChatType: undefined,
+  selectedChatData: undefined,
+  selectedChatMessages: [],
+  directMessagesContacts: [],
 
-        addMessage:(message)=>{
-            const selectedChatMessages = get().selectedChatMessages;
-            const selectedChatType = get().selectedChatType;
+  isUploading: false,
+  isDownloading:false,
+  fileUploadProgress: 0,
+  fileDownloadProgress: 0,
+  setIsUploading: (isUploading) => set({isUploading}),
+  setIsDownloading: (isDownloading) => set({isDownloading}),
+  setFileUploadProgress: (fileUploadProgress) => set({fileUploadProgress}),
+  setFileDownloadProgress: (fileDownloadProgress)=> set({fileDownloadProgress}),
+  setSelectedChatType: (selectedChatType) => set({ selectedChatType }),
+  setSelectedChatData: (selectedChatData) => set({ selectedChatData }),
+  setSelectedChatMessages: (selectedChatMessages) =>
+    set({ selectedChatMessages }),
+  setDirectMessagesContacts: (directMessagesContacts) =>
+    set({ directMessagesContacts }),
+  closeChat: () =>
+    set({
+      selectedChatData: undefined,
+      selectedChatType: undefined,
+      selectedChatMessages: [],
+    }),
 
-            set({
-                selectedChatMessages: [
-                    ...selectedChatMessages,
-                    {
-                        ...message,
-                        recipient:
-                            selectedChatType === "channel"
-                            ? message.recipient
-                            :message.recipient._id,
-                        sender:
-                            selectedChatType === "channel"
-                            ? message.sender
-                            :message.sender._id,
-                    },
-                ],
-            });
+  addMessage: (message) => {
+    const selectedChatMessages = get().selectedChatMessages;
+    const selectedChatType = get().selectedChatType;
 
+    set({
+      selectedChatMessages: [
+        ...selectedChatMessages,
+        {
+          ...message,
+          recipient:
+            selectedChatType === "channel"
+              ? message.recipient
+              : message.recipient._id,
+          sender:
+            selectedChatType === "channel"
+              ? message.sender
+              : message.sender._id,
         },
+      ],
+    });
+  },
 });
