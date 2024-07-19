2c418fd37ab55a0bd8aedbff956c50c98ecafc85
dgdurgesh <142913115+durgesh-h@users.noreply.github.com>
2024-07-19 09:32:53 +0530

    Channels mode is created

--- clients/src/context/SocketContext.jsx
+++ clients/src/context/SocketContext.jsx
@@ -1,51 +1,67 @@
 import { useAppStore } from "@/store";
 import { HOST } from "@/utils/constants";
 import { createContext, useContext, useRef, useEffect } from "react";
-import {io} from 'socket.io-client'
-
+import { io } from "socket.io-client";
 
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
-    return useContext(SocketContext);
+  return useContext(SocketContext);
 };
 
-export const SocketProvider = ({children}) =>{
-    const socket = useRef();
-    const {userInfo} = useAppStore();
+export const SocketProvider = ({ children }) => {
+  const socket = useRef();
+  const { userInfo } = useAppStore();
 
-    useEffect(()=> {
-        if(userInfo){
-            socket.current=io(HOST, {
-                withCredentials: true,
-                query: {userId: userInfo.id},
-            });
-            socket.current.on("connect", ()=> {
-                console.log("Connected to socket server");
-            });
+  useEffect(() => {
+    if (userInfo) {
+      socket.current = io(HOST, {
+        withCredentials: true,
+        query: { userId: userInfo.id },
+      });
+      socket.current.on("connect", () => {
+        console.log("Connected to socket server");
+      });
 
-            const handleRecieveMessage = (message)=> {
-                const {selectedChatData, selectedChatType, addMessage} = useAppStore.getState();
+      const handleRecieveMessage = (message) => {
+        const { selectedChatData, selectedChatType, addMessage } =
+          useAppStore.getState();
 
-                if(
-                    selectedChatType !== undefined &&
-                    (selectedChatData._id === message.sender._id || selectedChatData._id === message.recipient._id)
-                ){
-                    console.log("message recieved", message);
-                   addMessage(message); 
-                }
-            };
-
-            socket.current.on("recieveMessage", handleRecieveMessage);
-
-            return ()=> {
-                socket.current.disconnect();
-            };
+        if (
+          selectedChatType !== undefined &&
+          (selectedChatData._id === message.sender._id ||
+            selectedChatData._id === message.recipient._id)
+        ) {
+          console.log("message recieved", message);
+          addMessage(message);
         }
-    }, [userInfo]);
-    return(
-        <SocketContext.Provider value={socket.current} >
-            {children}
-        </SocketContext.Provider>
-    )
-}
+      };
+
+      const handleRecieveChannelMessage = (message) => {
+        const { selectedChatData, selectedChatType, addMessage } =
+          useAppStore.getState();
+
+        if (
+          selectedChatType !== undefined &&
+          selectedChatData._id === message.channelId
+        ) {
+          addMessage(message);
+        }
+      };
+
+      socket.current.on("recieveMessage", handleRecieveMessage);
+      socket.current.on(
+        "recieve-channel-message",
+        handleRecieveChannelMessage
+      );
+      return () => {
+        socket.current.disconnect();
+      };
+    }
+  }, [userInfo]);
+  return (
+    <SocketContext.Provider value={socket.current}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
--- clients/src/pages/chat/components/contacts-container/index.jsx
+++ clients/src/pages/chat/components/contacts-container/index.jsx
@@ -1,14 +1,17 @@
 import bhellogo from '@/assets/bhellogo.png'
 import ProfileInfo from './components/profile-info';
-import NewDM from '@/pages/auth/new-dm';
+import NewDM from './components/new-dm';
 import { useEffect } from 'react';
 import { apiClient } from '@/lib/api-client';
-import { GET_DM_CONTACTS_ROUTES } from '@/utils/constants';
+import { GET_DM_CONTACTS_ROUTES, GET_USER_CHANNELS_ROUTE } from '@/utils/constants';
 import { useAppStore } from '@/store';
 import ContactList from '@/components/contact-list';
+import CreateChannel from './components/create-channel';
+
+
 function ContactsContainer() {
 
-const {setDirectMessagesContacts, directMessagesContacts} = useAppStore();
+const {setDirectMessagesContacts, directMessagesContacts, channels, setChannels} = useAppStore();
 
 useEffect(()=>{
   const getContacts= async()=> {
@@ -19,8 +22,17 @@ useEffect(()=>{
       setDirectMessagesContacts(response.data.contacts);
     }
   };
+  const getChannels= async()=> {
+    const response = await apiClient.get(GET_USER_CHANNELS_ROUTE,{
+      withCredentials: true,
+    });
+    if(response.data.channels){
+      setChannels(response.data.channels);
+    }
+  };
   getContacts();
-}, []);
+  getChannels();
+}, [setChannels, setDirectMessagesContacts]);
 
   return (
     <div className="relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b] w-full"
@@ -40,6 +52,10 @@ useEffect(()=>{
         <div className='my-5'>
             <div className='flex items-center justify-between pr-10'>
             <Title text="Channels"/>
+            <CreateChannel/>
+            </div>
+            <div className='max-h-[38vh] overflow-y-auto scrollbar-hidden'>
+              <ContactList contacts={channels} isChannel={true}/>
             </div>
         </div>
         <ProfileInfo/>
