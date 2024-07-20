590f21b7f345fd49c51e7021053c4fd08da8788a
dgdurgesh <142913115+durgesh-h@users.noreply.github.com>
2024-07-20 08:43:31 +0530

    message bar fixed

--- clients/src/pages/chat/components/chat-container/components/message-bar/index.jsx
+++ clients/src/pages/chat/components/chat-container/components/message-bar/index.jsx
@@ -108,7 +108,7 @@ function MessageBar() {
   };
 
   return (
-    <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-12 gap-2 md:gap-4">
+    <div className="h-[10vh] bg-[#1c1d25] flex justify-center items-center px-8 mb-6 gap-2 md:gap-4">
       <div className="flex-1 flex bg-[#2a2b33] rounded-md items-center gap-5 pr-1 md:pr-8">
         <input
           type="text"
